db1393b8d9878b184b3451f03dd188f16cd39d90
Crissy <crystalgrant82@gmail.comgit config --global user.email crystalgrant82@gmail.com>
2018-09-28 23:23:51 -0400

    Adding business profile and review page

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,43 +1,39 @@
 import React from 'react';
 import './Business.css';
+import businessProfile from '../businessProfile/businessProfile';
 import Yelp from '../../util/Yelp';
-import { Link } from 'react-router-dom';
+import { Link, Route } from 'react-router-dom';
 
 
 class Business extends React.Component {
-  constructor(){
-    super();
-
-    this.state= {
-
-    }
-  }
-
-
   render(){
     const { id,imageSrc, name, address, city, state, zipCode, category, rating, reviewCount} = this.props.business
     return(
       <div className="Business">
-      <hr />
-        <div className="image-container">
-          <img src={imageSrc} alt="" />
-
-        </div>
-        <div className="vertical_content">
-          <Link to={`/businesses/${id}`}>{name}</Link>
-          <div className="Business-information">
-          <div className="Business-reviews">
-            <h3>{category.toUpperCase()}</h3>
-            <h3 className="rating">{`${rating} stars`} | {`${reviewCount} reviews`}</h3>
-          </div>
-          <br />
-          <div className="Business-address">
-            <p>{address}, {city}, {state} {zipCode}</p>
-          </div>
-
-          </div>
-        </div>
-        </div>
+            <hr />
+            <div className="image-container">
+                  <img src={imageSrc} alt="" />
+            </div>
+            <div className="vertical_content">
+                  <Link to={`/businesses/${id}`}>
+                        {name}
+                  </Link>
+                  <div className="Business-information">
+                        <div className="Business-reviews">
+                              <h3>
+                                  {category.toUpperCase()}
+                              </h3>
+                              <h3 className="rating">
+                                  {`${rating} stars`} | {`${reviewCount} reviews`}
+                              </h3>
+                        </div>
+                              <br />
+                        <div className="Business-address">
+                              <p>{address}, {city}, {state} {zipCode}</p>
+                        </div>
+                  </div>
+            </div>
+      </div>
     );
   }
 }
