f176ce1b96857b3e51887b6edfe0e9bd5ccf9f24
Crissy <crystalgrant82@gmail.comgit config --global user.email crystalgrant82@gmail.com>
2018-10-10 22:43:46 -0400

    featured css

--- src/components/Suggestions/SuggestionCards/HotAndNew.js
+++ src/components/Suggestions/SuggestionCards/HotAndNew.js
@@ -4,22 +4,41 @@ import '../Suggestions.css';
 
 const HotAndNew = ({hotEvent, description, id, image_url, name, category, distance }) => (
   <div>
-    <a href={hotEvent.event_site_url} target='blank'>
       <div className='card' key={hotEvent.id}>
-        <div className='card__img'>
-          <img src={hotEvent.image_url} />
-        </div>
-        <div className='card__header'>
-            <h4>{hotEvent.name}</h4>
-        </div>
-        <div>
-          {Math.round(hotEvent.distance * 0.00062137) / 100} miles
-        </div>
-        <div>
-          <p>Category: {hotEvent.category}</p>
-        </div>
-      </div>
-    </a>
+        <a href={hotEvent.url} target='blank'>
+          <div className='card__img'>
+            <img src={hotEvent.image_url} />
+          </div>
+        </a>
+          <div className='card_info'>
+            <div className='card__header'>
+                  <a href={hotEvent.url} target='blank'>{hotEvent.name}</a>
+            </div>
+            <div>
+
+              {/* {Math.round(hotEvent.distance * 0.00062137) / 100} miles */}
+            </div>
+              <div>
+                {
+                  hotEvent.categories.map((cat, i) => (
+
+                  hotEvent.categories.length -1 ?
+                  (<div className='card_info_cat' key={i}>
+                    {cat.title}
+                      </div>)
+                    :
+                  (<div className='card_info_cat' key={i}>
+                    {cat.title}
+                      </div>)
+
+                ))}
+              </div>
+
+
+          </div>
+            </div>
+
+
     </div>
 )
 
