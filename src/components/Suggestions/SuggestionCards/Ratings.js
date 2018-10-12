8a9b513cdb52dd2a5e126041cf4220fc0ea08e72
Crissy <crystalgrant82@gmail.comgit config --global user.email crystalgrant82@gmail.com>
2018-10-11 23:03:02 -0400

    Hot and New and Featured css text layout

---
+++ src/components/Suggestions/SuggestionCards/Ratings.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './Ratings.css';
+
+class Ratings extends Component{
+  constructor(props) {
+    super(props);
+  }
+
+  render(){
+    let stars = []
+    let remander = this.props.rating % 1
+    for(let i = 1; i <= 5; i++){
+      let classRating = "star_rating";
+
+      if(this.props.rating >= i && this.props.rating !== null){
+        classRating += 'star_rating full_star'
+      }
+      if(remander > 0){
+        classRating += 'star_rating half_star'
+      }
+
+      stars.push(
+        <label className={classRating} key={i}>
+          ★
+        </label>
+      )
+
+    }
+    return(
+      <div>
+        {stars}
+      </div>
+    )
+  }
+}
+
+export default Ratings;
